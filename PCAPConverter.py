062e4f634a2df3bae7c8d6941f43cd5450663209
robert-gaines <robert.gaines@hushmail.com>
2022-03-01 21:48:57 -0800

    'Test'

--- PCAPConverter.py
+++ PCAPConverter.py
@@ -11,15 +11,40 @@ except Exception as e:
 class Window(QWidget):
 
     def __init__(self):
+        #
         QWidget.__init__(self)
         QLabel.__init__(self)
         #
         self.setWindowTitle('PCAP Converter')
-        self.setGeometry(800,100,500,800)
+        self.setGeometry(500,100,300,300)
         #
-        button = QPushButton("Upload", self)
-        button.clicked.connect(self.GetFile)
+        self.upload_button = QPushButton("Upload PCAP File", self)
+        self.upload_button.clicked.connect(self.GetFile)
         #
+        self.parse_button = QPushButton("Convert PCAP", self)
+        self.parse_button.clicked.connect(self.GetFile)
+        #
+        self.format_label      = QLabel("Format")        
+        self.format_combo_box  = QComboBox() 
+        self.format_combo_box.addItem('CSV')
+        self.format_combo_box.addItem('XLSX')
+        self.format_combo_box.addItem('TXT')  
+        #
+        main_layout              = QFormLayout()
+        self.vertical_button_box = QVBoxLayout()
+        #
+        self.vertical_button_box.addWidget(self.upload_button)
+        self.vertical_button_box.addWidget(self.format_label)
+        self.vertical_button_box.addWidget(self.format_combo_box)
+        self.vertical_button_box.addWidget(self.parse_button)
+        main_layout.addRow(self.vertical_button_box)
+        #
+        self.setLayout(main_layout)
+
+    def GetFile(self):
+        dialog = QFileDialog()
+        fname = dialog.getOpenFileName(None, "Import CSV", "", "PCAP Data Files (*.pcap)")
+        print(fname)
 
 
 if(__name__ == '__main__'):
